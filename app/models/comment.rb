651dbe79a7dbfe8330e72ac16ec47cdcecfd5629
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2017-03-15 14:33:17 -0400

    add validations for post and comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,13 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  valdidates :body, presence: true
+  validates :post, presence: true
+
+  # validtes :check_blank
+
+  # def check_blank
+  # 	if self.body
+  # end
 end
--- db/migrate/20170310183930_create_posts.rb
+++ db/migrate/20170310183930_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
       t.string :title
-      t.text :body
+      t.text :body, null: false
       t.references :user, foreign_key: true, null: false
 
       t.timestamps null: false
