4315245eb2a8458288eb5fbb35a9e2d5bc234268
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2017-03-15 17:23:58 -0400

    add logout feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, except: [:index]
 
-  root 'users#new'
+  delete 'logout', to: 'sessions#destroy'
+
+  root 'posts#index'
 end
