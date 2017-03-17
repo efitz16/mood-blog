de9a21026139db9552b8e1734a14a3459e2bd001
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2017-03-17 16:57:02 -0400

    Begin working on posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,27 @@
 class PostsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  	redirect_to root_url if !logged_in?
+  	@post = Post.new
+  end
+
+  def create
+  	@post = Post.new(post_params)
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, except: [:index]
 
+  resources :posts
+
   get 'login', to: 'sessions#new'
 
   post 'login', to: 'sessions#create'
