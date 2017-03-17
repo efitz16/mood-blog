aabe07edd219a1b5f3a10639c8d98aef4dddeb7c
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2017-03-16 22:20:46 -0400

    Create sessions new and create methods; working on login form and routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
+  def new
+  	redirect_to root_url if logged_in?
+
+  	#redirect to user page
+  end
+
+  def create
+  	
+  end
+
   def destroy
   	log_out if logged_in?
   	redirect_to root_url
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, except: [:index]
 
+  get 'login', to: 'sessions#new'
+
   delete 'logout', to: 'sessions#destroy'
 
   root 'posts#index'
