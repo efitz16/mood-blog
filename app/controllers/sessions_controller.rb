84f8d55a9ef36792d0397da7b4780ee84b564595
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2017-03-17 01:15:58 -0400

    Finish basic user login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,24 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	
+  	@user = User.find_by(email: session_params[:email].downcase)
+
+  	if @user && @user.authenticate(session_params[:password])
+      log_in(@user)
+      redirect_to root_url
+  	else
+  	  flash[:notice] = "Error logging in"
+  	  render 'new'
+  	end
   end
 
   def destroy
   	log_out if logged_in?
   	redirect_to root_url
+  end
+
+  private
+  def session_params
+  	params.require(:session).permit(:email, :password)
   end
 end
