b3c6379b8d5a28b53839881cbb42cde19e5ea0df
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2017-03-23 18:44:43 -0400

    generate comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'comments/index'
+
+  get 'comments/show'
+
+  get 'comments/new'
+
+  get 'comments/create'
+
+  get 'comments/edit'
+
+  get 'comments/update'
+
+  get 'comments/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, except: [:index] do
   	resources :posts, only: [:index]
