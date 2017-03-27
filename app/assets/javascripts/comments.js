f7501243e8cef2e6d29dfebcc0c9b65772511331
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2017-03-26 22:37:33 -0400

    ajax comments for the comments index and post show--would like to clean this up later

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
-  // $('#post-comments-link').on("click", function(e) {
-  //   e.preventDefault();
+  $('#show-post-page-comment-link').on("click", function(e) {
+    e.preventDefault();
 
-  //   var target = e.target;
+    var target = e.target;
+    var jqTarget = $(target);
 
-  //   $.ajax({
-  //     url: target.href,
-  //     method: 'get'
-  //   })
-  //   .done(function(response) {
-  //     $(target).hide();
-  //     $('.posts-container').after(response);
-
-
-  //   })
-  //   .fail(function(e){
-  //     console.log(error);
-  //   });
-  // });
+    $.ajax({
+      url: target.href,
+      method: 'get'
+    })
+    .done(function(response) {
+      jqTarget.hide();
+      $('#show-post').after(response);
+      jqTarget.show();
+    })
+    .fail(function(e){
+      console.log(error);
+    });
+  });
 
     $('#add-comment-link').on("click", function(e) {
       e.preventDefault();
@@ -35,7 +35,21 @@ $(document).ready(function() {
       });
     });
 
-    // submit form
+    $('.one-post').on("click", '#add-comment-link', function(e) {
+      e.preventDefault();
+
+      var target = e.target;
+      var jqTarget = $(target);
+
+      $.ajax({
+        url: target.href,
+        method: 'get'
+      })
+      .done(function(response) {
+        jqTarget.hide();
+        $('.comment-form-container-ajax').prepend(response);
+      });
+    });
 
     $('.comment-form-container-ajax').on("submit", "#comment-form", function(e){
       e.preventDefault();
@@ -53,7 +67,25 @@ $(document).ready(function() {
         $('#all-comments-container').append(response);
         $('#add-comment-link').show();
       });
-    })
+    });
+
+    $('.one-post').on("submit", "#comment-form", function(e){
+      e.preventDefault();
+
+      var target = e.target;
+      var jqTarget = $(target);
+
+      $.ajax({
+        url: target.action,
+        method: 'post',
+        data: jqTarget.serialize()
+      })
+      .done(function(response) {
+        jqTarget.hide();
+        $('#all-comments-container').append(response);
+        $('#add-comment-link').show();
+      });
+    });
 
 
 });
